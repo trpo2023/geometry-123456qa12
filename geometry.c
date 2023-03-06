90fa0d6e7bb35f0242654351fb46c68a1666eb72
Щукин <egorshukin1235@gmail.com>
2023-03-06 08:05:30 +0300

    Change input method to circle(x y, r)

--- geometry.c
+++ geometry.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 void circleperimeter(float p){
 	printf("perimeter = %f\n", 2*M_PI*p);
 }
@@ -11,36 +12,53 @@ void intersects(float xl1, float yl1, float radl1, float xl2, float yl2, float r
 	float ylen=fabs(yl1-yl2);
 	float distance=sqrt(xlen*xlen+ylen*ylen);
 	if (distance-radl1-radl2<=0){
-		printf("%d.circle\n", n+1);
+		printf("	%d.circle\n", n+1);
 	}
 }
 int main(){
-	int circlenumber;
-	float x,y,rad;
-	printf("Введите количество кругов ");
-	scanf ("%d", &circlenumber);
-	float centerx[circlenumber];
-	float centery[circlenumber];
-	float radius[circlenumber];
-	for (int q=0; q<circlenumber; q++){
-		printf("Введите координату x центра %d круга ", q+1);
-		scanf("%f", &x);
-		centerx[q]=x;
-		printf("Введите координату y центра %d круга ", q+1);
-		scanf("%f", &y);
-		centery[q]=y;
-		printf("Введите радиус %d круга ", q+1);
-		scanf("%f", &rad);
-		radius[q]=rad;
+	int circlenumber=0;
+	char input[50];
+	float x, y, rad;
+	float centerx[100];
+	float centery[100];
+	float radius[100];
+	int start=1;
+	while(1) {
+		if (start==1){
+			printf("Введите stop, чтобы закончить ввод\n");
+			start=0;
+		}
+		if(fgets(input, sizeof(input), stdin) == NULL) {
+			break;
+		}
+		if(strncmp(input, "stop", 4) == 0) {
+			break;
+		}
+		if(sscanf(input, "circle(%f %f, %f)", &x, &y, &rad) != 3) {
+			printf("Ошибка ввода.\n");
+		} else {
+			centerx[circlenumber]=x;
+			centery[circlenumber]=y;
+			radius[circlenumber]=rad;
+			circlenumber+=1;
+		}
 	}
-	for (int i=0; i<circlenumber; i++){
-		printf("%d.circle(%f, %f %f)\n", i+1, centerx[i], centery[i], radius[i]);
-		circleperimeter(radius[i]);
-		circlearea(radius[i]);
+	float cx[circlenumber];
+	float cy[circlenumber];
+	float crad[circlenumber];
+	for (int c=0; c!=circlenumber; c++){
+		cx[c]=centerx[c];
+		cy[c]=centery[c];
+		crad[c]=radius[c];
+	}
+	for (int i=0; i!=circlenumber; i++){
+		printf("%d.circle(%f, %f %f)\n", i+1, cx[i], cy[i], crad[i]);
+		circleperimeter(crad[i]);
+		circlearea(crad[i]);
 		printf("intersects:\n");
 		for (int j=0; j<circlenumber; j++){
 			if (i != j) {
-				intersects(centerx[i], centery[i], radius[i], centerx[j], centery[j], radius[j], j);
+				intersects(cx[i], cy[i], crad[i], cx[j], cy[j], crad[j], j);
 			}
 		}
 	}
